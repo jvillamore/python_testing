94580747e2e8429f5086d6b47da852f4052b8280
Juan Villafuerte Mollinedo <villafuerte.j@outlook.com>
2025-04-01 20:41:18 -0400

    Nuevos asserts

--- test/test_bank.py
+++ test/test_bank.py
@@ -20,14 +20,14 @@ class BankAccountTests(unittest.TestCase):
 
     def test_deposit(self):
         new_balance = self.account.deposit(500)
-        assert new_balance == 1500
+        self.assertEqual(new_balance, 1500, "El balance no igual")
 
     def test_withdraw(self):
         new_balance = self.account.withdraw(200)
-        assert new_balance == 800
+        self.assertEqual(new_balance, 800, "El balance no igual")
 
     def test_get_balance(self):
-        assert self.account.get_balance() == 1000
+        self.assertEqual(self.account.get_balance(), 1000)
 
     def test_transfer(self):
         target_account = BankAccount(balance=500)
@@ -42,7 +42,7 @@ class BankAccountTests(unittest.TestCase):
 
     def test_transaction_log(self):
         self.account.deposit(500)
-        assert os.path.exists("transaction_log.txt")
+        self.assertTrue(os.path.exists("transaction_log.txt"))
 
     def test_count_transactions(self):
         assert self._count_lines(self.account.log_file) == 1
