f2291c5555e9bc4893b4438aa3ca0094e8ab957b
Juan Villafuerte Mollinedo <villafuerte.j@outlook.com>
2025-04-01 20:20:25 -0400

    Pruebas unitarias 02

--- test/test_bank.py
+++ test/test_bank.py
@@ -1,4 +1,5 @@
 import unittest
+import os
 
 from src.bank_account import BankAccount
 
@@ -6,7 +7,16 @@ from src.bank_account import BankAccount
 class BankAccountTests(unittest.TestCase):
 
     def setUp(self) -> None:
-        self.account = BankAccount(balance=1000)
+        self.account = BankAccount(balance=1000,
+                                   log_file="transaction_log.txt")
+
+    def tearDown(self) -> None:
+        if os.path.exists(self.account.log_file):
+            os.remove(self.account.log_file)
+
+    def _count_lines(self, filename):
+        with open(filename, "r") as f:
+            return len(f.readlines())
 
     def test_deposit(self):
         new_balance = self.account.deposit(500)
@@ -21,7 +31,6 @@ class BankAccountTests(unittest.TestCase):
 
     def test_transfer(self):
         target_account = BankAccount(balance=500)
-        self.account.balance = 1000
         new_balance = self.account.transfer(500, target_account)
         assert new_balance == 500
         assert target_account.balance == 1000
@@ -30,3 +39,12 @@ class BankAccountTests(unittest.TestCase):
         target_account = BankAccount(balance=500)
         with self.assertRaises(ValueError):
             self.account.transfer(1500, target_account)
+
+    def test_transaction_log(self):
+        self.account.deposit(500)
+        assert os.path.exists("transaction_log.txt")
+
+    def test_count_transactions(self):
+        assert self._count_lines(self.account.log_file) == 1
+        self.account.deposit(500)
+        assert self._count_lines(self.account.log_file) == 2
